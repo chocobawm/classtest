553c1e4d765cc027285a8c50c69967bd5dca960a
Tom <tom.wat0605@gmail.com>
2020-04-07 15:48:06 +0100

    Commit2

--- classproject/news/models.py
+++ classproject/news/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class newStory(models.Model):
+    heading_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    story_text = models.CharField(max_length=5000)
