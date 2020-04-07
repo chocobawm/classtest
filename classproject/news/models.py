d12b5d3e80db6d17efbb277804b1001097ac8d04
Tom <tom.wat0605@gmail.com>
2020-04-07 16:56:55 +0100

    Some commits
    Had problems with the model

--- classproject/news/models.py
+++ classproject/news/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class newStory(models.Model):
+class NewStory(models.Model):
     heading_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     story_text = models.CharField(max_length=5000)
